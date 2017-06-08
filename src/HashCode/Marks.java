27f484f2093c3515f539b2a48867fa1f566216e7
Nitish2 <nitishn886@gmail.com>
2017-06-08 15:45:54 +0530

    Assignment6.2

---
+++ src/HashCode/Marks.java
@@ -0,0 +1,17 @@
+package HashCode;
+
+public class Marks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Marks m1= new Marks();
+		Marks m2= new Marks();
+		 System.out.println(m1.hashCode());  // prints the hashCode of m1
+		 System.out.println(m2.hashCode());  //prints the hashCode of m2
+		 
+		 
+
+	}
+
+}
